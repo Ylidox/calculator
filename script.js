d07755d851389fd1cd49f1263b579eaf5b3a316d
Aliev Artiom <89365793+Ylidox@users.noreply.github.com>
2022-10-14 09:45:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -58,8 +58,11 @@ class Controller{
 		// if(symb != "0") this.numbers[this.pointer] = +this.numbers[this.pointer];
 		if(symb == "0"){
 			if(typeof(number) == 'string') this.numbers[this.pointer] += "" + symb;
+			else if(this.numbers[this.pointer] !== 0) this.numbers[this.pointer] += "" + symb;
 		}
-		else this.numbers[this.pointer] += "" + symb;
+		else{
+			this.numbers[this.pointer] += "" + symb;
+		} 
 
 		if(symb != '0') this.numbers[this.pointer] = +this.numbers[this.pointer];
 	}
@@ -155,6 +158,8 @@ class Interface{
 		}
 
 		addEventListener('keypress', (event) => {
+			//event.stopPropagation();
+			//event.preventDefault();
 			switch(event.key){
 			case "0": case "1":
 			case "2": case "3":
@@ -186,7 +191,13 @@ class Interface{
 				this.input.value = controller.getOutput();
 				break;
 			}
-
+			for(let item of this.numbers){
+				item.blur();
+			}
+	
+			for(let item of this.staff){
+				item.blur();
+			}
 		});
 	}
 }
@@ -195,3 +206,85 @@ let controller = new Controller();
 let interface = new Interface();
 interface.listen(controller);
 
+let input = (markup) => {
+	let controller = new Controller();
+
+	for(let i = 0; i < markup.length; i++){
+		switch(markup[i]){
+			case "0": case "1":
+			case "2": case "3":
+			case "4": case "5":
+			case "6": case "7":
+			case "8": case "9":
+				controller.setSymbol("number", markup[i]);
+				break;
+			case "c":
+				controller.setSymbol("stuff", "clear");
+				break;
+			case "+": case "-":
+			case "*": case "/":
+				controller.setSymbol("stuff", markup[i]);
+				break;
+			case "s":
+				controller.setSymbol("stuff", "signChange");
+				break;
+			case ".":
+				controller.setSymbol("stuff", "point");
+				break;
+			case "=":
+				controller.setSymbol("stuff", "equality");
+				break;
+		}
+	}
+
+	return controller;
+}
+
+let compare = (a, b) => {
+	if(typeof(a) != typeof(b)) return false;
+	if(typeof(a) == 'number'){
+		a *= 1000000;
+		b *= 1000000;
+		return (Math.round(a) == Math.round(b));
+	}
+	else return a === b;
+}
+
+let testCalculate = (markup, result) => {
+	let controller = input(markup);
+
+	if(compare( controller.result, result)){
+		console.log("success", markup, result);
+	}
+	else console.log("FAILED", markup, result, controller);
+}
+
+let testInput = (markup, num, action) => {
+	let [a, b] = num;
+	let controller = input(markup);
+	
+
+	let [n1, n2] = controller.numbers;
+	if(a === n1 && b === n2 && action === controller.action){
+		console.log("success", markup, num, action);
+	}else{
+		console.log("FAILED", controller, markup, num, action);
+	}
+}
+
+/*testInput("1200.12s-.0002", [-1200.12, 0.0002], "-");
+testInput("......12+.0000", [12, "0.0000"], "+");
+testInput("12.2+.....000012", [12.2, 0.000012], "+");
+testInput("0000012.002+00000012.3", [12.002, 12.3], "+");
+testInput("12c.02-12.1", [0.02, 12.1], "-");
+testInput("12.1+0.1c2.2+2", [2.2, 2], "+");
+testInput("12.2+0.2sss", [12.2, -0.2], "+");
+
+testCalculate("12.1s+12.2=", 0.1);
+testCalculate(".1+.2=", 0.3);
+testCalculate(".0000+.0000", 0);
+testCalculate("12.1-1s2.2=", 24.3);
+testCalculate("12/0=", 0);
+testCalculate("21/3=", 7);
+testCalculate("12.1*2s=", -24.2);
+testCalculate("12.1*0.00000=", 0);*/
